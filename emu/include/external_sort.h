e9743a0fab3cdbd5a7c9cc8940e87a6f1eb94f97
aparnasasidharan2017 <asasidharan@iit.edu>
2023-05-23 13:49:29 -0500

    extsort

--- emu/include/external_sort.h
+++ emu/include/external_sort.h
@@ -7,20 +7,25 @@
 
 class hdf5_sort
 {
+   private: 
+	int numprocs;
+	int myrank;
 
 
 
+  public: 
+       hdf5_sort(int n,int p) : numprocs(p), myrank(p)
+       {
 
+       }
+       ~hdf5_sort()
+       {
 
+       }
 
-
-
-
-
-
-
-
-
+       std::string merge_datasets(std::string &,std::string &);
+       std::string merge_stream_with_dataset(std::string &,std::vector<struct event>*);
+       std::string merge_multiple_dataset(std::vector<std::string>&);
 };
 
 #endif
--- emu/srcs/external_sort.cpp
+++ emu/srcs/external_sort.cpp
@@ -1,1 +1,35 @@
 #include "external_sort.h"
+
+std::string hdf5_sort::merge_datasets(std::string &s1,std::string &s2)
+{
+    std::string s3 = "merged_file";
+
+
+
+
+
+    return s3;
+}
+
+std::string hdf5_sort::merge_stream_with_dataset(std::string &s,std::vector<struct event>* events)
+{
+   std::string s2 = "merged_file";
+
+
+
+   return s2;
+
+}
+       
+std::string hdf5_sort::merge_multiple_dataset(std::vector<std::string>& snames)
+{
+    std::string s2 = "merged_file";
+
+
+
+
+
+
+   return s2;
+}
+
